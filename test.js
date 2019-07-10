4bab538743cdc133f1135b90c2065f8c65de3871
Anton <sadnessbiblethump@gmail.com>
2019-07-10 14:06:11 +0300

    Added some code in test.js

--- test.js
+++ test.js
@@ -1,1 +1,3 @@
-console.log('sad')
+getOneType = (arr, type) => arr.filter(el => typeof el == type)
+const someArr = [1, true, false, undefined, NaN, null, 'hello', true, 'text', true];
+console.log(getOneType(someArr, 'number'))
