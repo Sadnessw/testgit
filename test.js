9e3608e8343c01b3e3fbe9122e5943e86bd30086
Sadness <48298082+Sadnessw@users.noreply.github.com>
2019-07-10 14:29:32 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,4 @@
 getOneType = arr => arr.filter(el => isNaN(el) != true)
 const someArr = [1, true, false, undefined, NaN, null, 'hello', true, 'text', true, 2, 15];
 console.log(getOneType(someArr))
+#Solved
